a530044de5bf4934cf9246fc4871726db9aabe12
mcao <mcao@g-wearables.com>
2015-11-02 12:37:14 +0800

    Initial Commit

---
+++ InverseKinematics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InverseKinematics
+//
+//  Created by MengTsao on 2/11/15.
+//  Copyright (c) 2015 MengTsao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
